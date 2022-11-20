8f6d5660e9585ddb76b54b31064e356e78677da6
rikin <rikinrathod77@gmail.com>
2022-11-20 23:30:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/Mad_Practical_3_21012012029/RegistrationActivity.kt
@@ -0,0 +1,32 @@
+package com.example.Mad_Practical_3_21012012029
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.Mad_Practical_3_21012012029.databinding.ActivityRegistrationBinding
+
+class RegistrationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        binding.bottomNavigationView.selectedItemId = R.id.bottom_nav_reg
+        binding.bottomNavigationView.setOnItemSelectedListener{it1->
+            when(it1.itemId)
+            {
+                R.id.bottom_nav_login->
+                {
+                    Intent(this,LoginActivity::class.java).also {startActivity(it)}
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+
+        binding.tvLogin.setOnClickListener{
+            Intent(this,LoginActivity::class.java).apply {startActivity(this)}
+        }
+    }
+}
